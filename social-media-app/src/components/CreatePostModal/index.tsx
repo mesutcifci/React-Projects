97190487e9fd3f087e6e0b7af44a2031389d37ac
mesut çifci <cfcmesut@gmail.com>
2022-03-25 21:14:18 +0300

    basic form element added

--- social-media-app/src/components/CreatePostModal/index.tsx
+++ social-media-app/src/components/CreatePostModal/index.tsx
@@ -13,10 +13,49 @@ const CreatePostModal = ({ isVisible }: ModalProps) => {
   const hideCreatePostModal = () => {
     dispatch(modalActions.hideModal());
   };
-  
+
   return (
-    <Modal visible={isVisible} centered onCancel={hideCreatePostModal}>
-      <Form></Form>
+    <Modal
+      visible={isVisible}
+      centered
+      onCancel={hideCreatePostModal}
+      footer={[
+        <Button key="submit" type="primary">
+          Submit
+        </Button>,
+      ]}
+    >
+      <Form name="addUser" labelCol={{ span: 5 }} wrapperCol={{ span: 20 }}>
+        <Form.Item
+          label="Owner"
+          name="owner"
+          rules={[
+            { required: true, message: "Please input username of owner!" },
+          ]}
+          className="!mt-6"
+        >
+          <Select defaultValue="Zhejiang">
+            <Select.Option value="Zhejiang">Zhejiang</Select.Option>
+            <Select.Option value="Jiangsu">Jiangsu</Select.Option>
+          </Select>
+        </Form.Item>
+        <Form.Item
+          label="Title"
+          name="title"
+          rules={[{ required: true, message: "Please input title!" }]}
+          className="!mt-6"
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          label="Body"
+          name="bıdy"
+          rules={[{ required: true, message: "Please input body!" }]}
+          className="!mt-6"
+        >
+          <Input.TextArea showCount maxLength={200} rows={4} />
+        </Form.Item>
+      </Form>
     </Modal>
   );
 };
