9ca1b89374af90c14c982235a501c1397f04d7c2
Mesut Çiftçi <cfcmesut@gmail.com>
2022-10-23 19:37:43 +0300

    Project Type and Sector dropdowns created

--- paint-calculator/src/components/FormRenderer/index.tsx
+++ paint-calculator/src/components/FormRenderer/index.tsx
@@ -1,3 +1,6 @@
+import { InputLabel, MenuItem, Select, SelectChangeEvent } from "@mui/material";
+import { Box, Stack } from "@mui/system";
+import { useEffect, useState } from "react";
 import {
   IColour,
   IProducts,
@@ -19,7 +22,75 @@ interface IFormProps {
 }
 
 const FormRenderer = ({ data }: IFormProps) => {
-  return <form></form>;
+  const { projectTypes, sectors, projectType_sector } = data;
+  const [selectedProjectType, setSelectedProjectType] = useState(
+    projectTypes[0].name
+  );
+
+  const [filteredSectors, setFilteredSectors] =
+    useState<IProjectTypeSector[]>();
+  const [selectedSector, setSelectedSector] = useState<ISector | undefined>(
+    sectors[0]
+  );
+  useEffect(() => {
+    const sectorsByProjectType = projectType_sector.filter(
+      (sector) => sector.type === selectedProjectType
+    );
+    const sectorWithCostMultiplier = sectors.find(
+      (sector) => sector.name === sectorsByProjectType[0].sector
+    );
+
+    setFilteredSectors(sectorsByProjectType);
+    setSelectedSector(sectorWithCostMultiplier);
+  }, [selectedProjectType]);
+
+  const handleClickSector = (event: SelectChangeEvent<string>) => {
+    const value = event.target.value;
+
+    const sectorWithCostMultiplier = sectors.find(
+      (sector) => sector.name === value
+    );
+
+    setSelectedSector(sectorWithCostMultiplier);
+  };
+
+  return (
+    <Box sx={{ width: "100%", maxWidth: "500px", mx: "auto", pt: "100px" }}>
+      <Stack direction="row" sx={{ flexWrap: "wrap", gap: "20px" }}>
+        <Stack direction="row" alignItems="center" spacing={1}>
+          <InputLabel id="projectTypes">Project Type: </InputLabel>
+          <Select
+            labelId="projectTypes"
+            id="projectTypes"
+            value={selectedProjectType}
+            onChange={(e) => setSelectedProjectType(e.target.value)}
+          >
+            {projectTypes.map((type) => (
+              <MenuItem key={type.name} value={type.name}>
+                {type.name}
+              </MenuItem>
+            ))}
+          </Select>
+        </Stack>
+
+        <Stack direction="row" alignItems="center" spacing={1}>
+          <InputLabel id="sectors">Sector: </InputLabel>
+          <Select
+            labelId="sectors"
+            id="sectors"
+            value={selectedSector?.name}
+            onChange={(event) => handleClickSector(event)}
+          >
+            {filteredSectors?.map((sector) => (
+              <MenuItem key={sector.sector} value={sector.sector}>
+                {sector.sector}
+              </MenuItem>
+            ))}
+          </Select>
+        </Stack>
+      </Stack>
+    </Box>
+  );
 };
 
 export default FormRenderer;
