83585221f14aa4fe7a7bd866ded30135d76a8a51
Mesut Çiftçi <cfcmesut@gmail.com>
2022-11-05 16:16:03 +0300

    todo item tests created

--- todo-app/src/components/TodoItem/index.js
+++ todo-app/src/components/TodoItem/index.js
@@ -53,6 +53,7 @@ const TodoItem = ({ data }) => {
       {!isEditable && (
         <span
           className={`${style.text} ${data.isCompleted && style.lineThrough}`}
+          data-testid="todoSpan"
         >
           {data.text}
         </span>
@@ -78,6 +79,7 @@ const TodoItem = ({ data }) => {
             fill="currentColor"
             viewBox="0 0 16 16"
             onClick={completeTodo}
+            data-testid="completeIcon"
           >
             <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
             <path d="M10.97 4.97a.235.235 0 0 0-.02.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05z" />
@@ -91,6 +93,7 @@ const TodoItem = ({ data }) => {
             fill="currentColor"
             viewBox="0 0 16 16"
             onClick={() => setIsEditable(true)}
+            data-testid="editIcon"
           >
             <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
             <path
@@ -107,6 +110,7 @@ const TodoItem = ({ data }) => {
             fill="currentColor"
             viewBox="0 0 16 16"
             onClick={handleDeleteTodo}
+            data-testid="deleteIcon"
           >
             <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z" />
           </svg>
@@ -140,6 +144,7 @@ const TodoItem = ({ data }) => {
             fill="currentColor"
             viewBox="0 0 16 16"
             onClick={() => setIsEditable(false)}
+            data-testid="closeIcon"
           >
             <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z" />
           </svg>
