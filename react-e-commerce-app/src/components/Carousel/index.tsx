24ad811da233a56d8e7bec083db684d29d9ace4e
Mesut Çiftçi <cfcmesut@gmail.com>
2022-11-17 00:32:05 +0300

    basic mobile design of slider completed

--- react-e-commerce-app/src/components/Carousel/index.tsx
+++ react-e-commerce-app/src/components/Carousel/index.tsx
@@ -1,6 +1,9 @@
 import { useState } from "react";
 import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
 
 interface ICarouselItemType {
   id: number;
@@ -18,28 +21,12 @@ interface ICarouselProps {
 const Carousel = ({ variant, data }: ICarouselProps) => {
   const [currentImageIndex, setCurrentImageIndex] = useState(1);
 
-  const handleClickForwardButton = () => {
-    const lastItemIndex = data.length;
-    setCurrentImageIndex((prevState) => {
-      // if user reach last image and click forward button show first image
-      if (prevState + 1 > lastItemIndex) {
-        return 1;
-      } else {
-        return prevState + 1;
-      }
-    });
-  };
-
-  const handleClickBackwardButton = () => {
-    const lastItemIndex = data.length;
-    setCurrentImageIndex((prevState) => {
-      // if user reach last image and click backward button show last image
-      if (prevState - 1 <= 0) {
-        return lastItemIndex;
-      } else {
-        return prevState - 1;
-      }
-    });
+  const updateCurrentImageIndex = (slideDirection: string) => {
+    if (slideDirection === "right") {
+      setCurrentImageIndex((prevState) => prevState + 1);
+    } else if (slideDirection === "left") {
+      setCurrentImageIndex((prevState) => prevState - 1);
+    }
   };
 
   return (
@@ -48,6 +35,7 @@ const Carousel = ({ variant, data }: ICarouselProps) => {
         display: "flex",
         position: "relative",
         height: "501px",
+        overflow: "hidden",
       }}
     >
       {data.map((item, index) => {
@@ -57,7 +45,12 @@ const Carousel = ({ variant, data }: ICarouselProps) => {
               flexShrink: "0",
               height: "501px",
               width: "100%",
-              "& img": { width: "100%", height: "100%", objectFit: "cover" },
+              "& img": {
+                width: "100%",
+                height: "100%",
+                objectFit: "cover",
+                filter: "grayScale(0.2)",
+              },
               "&.slide-item": {
                 transform: "translateX(0)",
                 position: "absolute",
@@ -73,17 +66,90 @@ const Carousel = ({ variant, data }: ICarouselProps) => {
                 : "slide-item "
             }`}
           >
+            <Typography
+              sx={{
+                position: "absolute",
+                fontSize: "43px",
+                fontWeight: "bold",
+                lineHeight: "42px",
+                color: "white",
+                transform: "translateY(-50%)",
+                top: "50%",
+                left: "35px",
+                zIndex: 2,
+                width: "80%",
+              }}
+            >
+              {item.text}
+            </Typography>
+            <Box
+              sx={{
+                position: "absolute",
+                bottom: "74px",
+                display: "flex",
+                columnGap: "22px",
+                zIndex: 2,
+                left: "35px",
+                alignItems: "center",
+              }}
+            >
+              <IconButton
+                sx={{
+                  color: "white",
+                  backgroundColor: "#FBB03B",
+                }}
+                component="button"
+              >
+                <ArrowForwardIcon />
+              </IconButton>
+              <Typography
+                sx={{ fontSize: "13px", color: "white", fontWeight: "bold" }}
+              >
+                SHOP NOW
+              </Typography>
+            </Box>
             <img src={item.src + (index + 1) + "." + item.imageType} />
           </Box>
         );
       })}
-      <Box sx={{ position: "absolute", bottom: "10px" }}>
-        <Button variant="contained" onClick={handleClickBackwardButton}>
-          Back
-        </Button>
-        <Button variant="contained" onClick={handleClickForwardButton}>
-          Forward
-        </Button>
+      <Box
+        sx={{
+          position: "absolute",
+          bottom: "10px",
+          right: "30px",
+          "& .Mui-disabled": {
+            backgroundColor: "rgba(255,255,255, 0.2) !important",
+            color: "white !important",
+          },
+        }}
+      >
+        <IconButton
+          sx={{
+            color: "black",
+            backgroundColor: "white",
+            borderRadius: "0",
+            marginRight: "5px",
+            "&:hover": { color: "black", backgroundColor: "white" },
+          }}
+          component="button"
+          onClick={() => updateCurrentImageIndex("left")}
+          {...(currentImageIndex === 1 && { disabled: true })}
+        >
+          <ArrowBackIcon />
+        </IconButton>
+        <IconButton
+          sx={{
+            color: "black",
+            backgroundColor: "white",
+            borderRadius: "0",
+            "&:hover": { color: "black", backgroundColor: "white" },
+          }}
+          component="button"
+          onClick={() => updateCurrentImageIndex("right")}
+          {...(currentImageIndex === data.length && { disabled: true })}
+        >
+          <ArrowForwardIcon />
+        </IconButton>
       </Box>
     </Box>
   );
