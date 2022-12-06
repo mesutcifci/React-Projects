f192b6cd14b7274b407c84db3e037b74ac653619
Mesut Çiftçi <cfcmesut@gmail.com>
2022-12-06 20:51:04 +0300

    react-e-commerce: - deleted redundant import from productSlice.ts file.   I will get products from firebase so I don't need to use constant for   it

--- react-e-commerce-app/src/features/products/productsSlice.ts
+++ react-e-commerce-app/src/features/products/productsSlice.ts
@@ -1,12 +1,11 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { IProduct } from "../../types/product";
-import { products } from "../../constants/products";
 
 interface IState {
   products: IProduct[];
 }
 
-const initialState: IState = { products: [...products] };
+const initialState: IState = { products: [] };
 
 const productsSlice = createSlice({
   name: "products",
