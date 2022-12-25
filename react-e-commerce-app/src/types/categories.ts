676c906fae3af7d4b8561c71a55d5082884bee07
Mesut Çiftçi <cfcmesut@gmail.com>
2022-12-25 20:31:03 +0300

    react-e-commerce: orderId and ProductAmount fields added to categories

--- react-e-commerce-app/src/types/categories.ts
+++ react-e-commerce-app/src/types/categories.ts
@@ -5,14 +5,17 @@ export interface IBase {
 }
 
 export interface ICategory extends IBase {
+  orderId: number;
   secondaryCategories: ISecondaryCategory[];
 }
 
 export interface ISecondaryCategory extends IBase {
   tertiaryCategories: ITertiaryCategory[];
+  productAmount: number;
 }
 
 export interface ITertiaryCategory extends IBase {
   id: string;
   name: string;
+  productAmount: number;
 }
