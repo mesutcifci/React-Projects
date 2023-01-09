b3a7a0ce060ba267922f803eee1ce4e7cb673d5e
Mesut Çiftçi <cfcmesut@gmail.com>
2023-01-09 15:10:24 +0300

    react-e-commerce: Stepper created for Cart component

--- react-e-commerce-app/src/pages/Cart/index.tsx
+++ react-e-commerce-app/src/pages/Cart/index.tsx
@@ -1,5 +1,9 @@
-import { ShoppingCart } from "@mui/icons-material";
-import { Step, Stepper } from "@mui/material";
+import {
+  LocalShippingOutlined,
+  PaymentOutlined,
+  ShoppingCartOutlined,
+} from "@mui/icons-material";
+import { Step, Stepper, Typography } from "@mui/material";
 import { Stack } from "@mui/system";
 import { useState } from "react";
 
@@ -17,14 +21,91 @@ const Cart = () => {
 
   const renderPageContent = () => {};
 
+  const renderStepperIcon = (index: number) => {
+    switch (index) {
+      case 0:
+        return <ShoppingCartOutlined />;
+      case 1:
+        return <LocalShippingOutlined />;
+      case 2:
+        return <PaymentOutlined />;
+    }
+  };
+
   return (
-    <Stack>
-      <Stack>
-        <Stepper activeStep={activeStep}>
+    <Stack
+      sx={{
+        paddingLeft: { xs: "16px", lg: "116px" },
+        paddingRight: { xs: "16px", lg: "116px" },
+      }}
+    >
+      <Stack
+        direction="row"
+        flexWrap="wrap"
+        alignItems="center"
+        columnGap="10px"
+        sx={{ justifyContent: { xs: "center", sm768: "space-between" } }}
+      >
+        <Typography
+          sx={{
+            display: "inline-flex",
+            alignItems: "center",
+            justifyContent: { xs: "center", sm768: "initial" },
+            fontSize: "20px",
+            fontWeight: "500",
+            maxWidth: { sm768: "288px" },
+            width: "100%",
+            height: "60px",
+            textAlign: { xs: "center", sm768: "initial" },
+          }}
+        >
+          {steps[activeStep]}
+        </Typography>
+        <Stepper
+          activeStep={activeStep}
+          sx={{
+            minWidth: "288px",
+            maxWidth: { xs: "450px", sm768: "320px" },
+            height: "60px",
+            width: "100%",
+          }}
+        >
           {steps.map((label, index) => {
             return (
-              <Step key={label} onClick={() => handleClickStepIcon(index)}>
-                <ShoppingCart />
+              <Step
+                key={label}
+                onClick={() => handleClickStepIcon(index)}
+                sx={{
+                  height: "36px",
+                  width: "36px",
+                  backgroundColor: `${
+                    index < activeStep
+                      ? "#ffffff"
+                      : index === activeStep
+                      ? "#FBB03B"
+                      : "unset"
+                  }`,
+                  border: `${
+                    index > activeStep ? "none" : "1px solid #FBB03B"
+                  }`,
+                  borderRadius: "100%",
+                  cursor: "pointer",
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+
+                  "& svg": {
+                    color: `${
+                      index < activeStep
+                        ? "#FBB03B"
+                        : index === activeStep
+                        ? "#ffffff"
+                        : "#D8D8D8"
+                    }`,
+                  },
+                }}
+              >
+                {renderStepperIcon(index)}
               </Step>
             );
           })}
