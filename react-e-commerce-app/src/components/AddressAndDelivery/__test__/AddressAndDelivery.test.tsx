af713b5eccad8a035da2f9099fdb22bd5360e31a
Mesut Çiftçi <cfcmesut@gmail.com>
2023-01-18 12:28:23 +0300

    react-e-commerce: new test cases created AddressAndDelivery

--- react-e-commerce-app/src/components/AddressAndDelivery/__test__/AddressAndDelivery.test.tsx
+++ react-e-commerce-app/src/components/AddressAndDelivery/__test__/AddressAndDelivery.test.tsx
@@ -2,15 +2,15 @@ import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import AddressDataAndDelivery from "../index";
 import deliveryMethods from "../../../constants/delivery.json";
 
-const inputLabelTexts = [
-  "First Name",
-  "Last Name",
-  "Address",
-  "City",
-  "Postal Code / ZIP",
-  "Phone Number",
-  "Country",
-  "Email",
+const inputData = [
+  { label: "First Name", error: "First name is required" },
+  { label: "Last Name", error: "Last name is required" },
+  { label: "Address", error: "Address is required" },
+  { label: "City", error: "City name is required" },
+  { label: "Postal Code / ZIP", error: "Postal code is required" },
+  { label: "Phone Number", error: "Phone number is required" },
+  { label: "Country", error: "Country is required" },
+  { label: "Email", error: "Email is required" },
 ];
 
 describe("AddressDataAndDelivery test", () => {
@@ -19,14 +19,14 @@ describe("AddressDataAndDelivery test", () => {
   });
 
   test("Input labels should be visible", () => {
-    inputLabelTexts.forEach((text) => {
-      expect(screen.getByText(text)).toBeInTheDocument();
+    inputData.forEach((data) => {
+      expect(screen.getByText(data.label)).toBeInTheDocument();
     });
   });
 
   test("Inputs should be visible", () => {
-    inputLabelTexts.forEach((text) => {
-      expect(screen.getByLabelText(text)).toBeInTheDocument();
+    inputData.forEach((data) => {
+      expect(screen.getByLabelText(data.label)).toBeInTheDocument();
     });
   });
 
@@ -37,14 +37,14 @@ describe("AddressDataAndDelivery test", () => {
   });
 
   test("Should be able to type in inputs", () => {
-    inputLabelTexts.forEach(async (text) => {
-      const inputElement: HTMLInputElement = screen.getByLabelText(text);
-      if (text === "Phone Number") {
+    inputData.forEach(async (data) => {
+      const inputElement: HTMLInputElement = screen.getByLabelText(data.label);
+      if (data.label === "Phone Number") {
         fireEvent.change(inputElement, {
           target: { value: `+90 534 404 44 44` },
         });
         expect(inputElement.value).toBe(`+90 534 404 44 44`);
-      } else if (text === "Country") {
+      } else if (data.label === "Country") {
         fireEvent.focus(inputElement);
         fireEvent.change(inputElement, {
           target: { value: `Tur` },
@@ -57,11 +57,27 @@ describe("AddressDataAndDelivery test", () => {
         // Check input adornment
         expect(screen.getAllByAltText("Turkey")).toBeInTheDocument();
       } else {
-        fireEvent.change(inputElement, { target: { value: `Test ${text}` } });
-        expect(inputElement.value).toBe(`Test ${text}`);
+        fireEvent.change(inputElement, {
+          target: { value: `Test ${data.label}` },
+        });
+        expect(inputElement.value).toBe(`Test ${data.label}`);
       }
     });
   });
 
-  // TODO test input error messages
+  test("Should not visible error messages on first render", () => {
+    inputData.forEach((data) => {
+      expect(screen.queryByText(data.error)).not.toBeInTheDocument();
+    });
+  });
+
+  test("Should error messages visible after onBlur state when fields are empty", () => {
+    inputData.forEach(async (data) => {
+      const inputElement = screen.getByLabelText(data.label);
+      fireEvent.focus(inputElement);
+      fireEvent.blur(inputElement);
+      const label = await waitFor(() => screen.getByText(data.error));
+      expect(label).toBeInTheDocument();
+    });
+  });
 });
