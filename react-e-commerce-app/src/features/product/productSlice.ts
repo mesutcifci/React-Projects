2d3f9322fcc6cf080f82e3ee03c9847d69d7c699
Mesut Çiftçi <cfcmesut@gmail.com>
2023-01-26 08:17:51 +0300

    react-e-commerce: unused packages removed

--- react-e-commerce-app/src/features/product/productSlice.ts
+++ react-e-commerce-app/src/features/product/productSlice.ts
@@ -1,4 +1,4 @@
-import { createSlice, createAsyncThunk, PayloadAction } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import { IProduct } from "../../types/product";
 import { doc, getDocFromServer } from "firebase/firestore";
 import { db } from "../../firebase";
