58634dde14c4ec86a934721b665dec275a40200f
Mesut Çiftçi <cfcmesut@gmail.com>
2024-01-11 00:33:08 +0300

    refactor(react-e-commerce/scroll-to-top): remove redundant scroll restoration configuration

--- react-e-commerce-app/src/components/ScrollToTop/index.tsx
+++ react-e-commerce-app/src/components/ScrollToTop/index.tsx
@@ -10,10 +10,6 @@ const ScrollToTop = ({ params }: IProps) => {
   const { pathname } = useLocation();
 
   useEffect(() => {
-    const canControlScrollRestoration = "scrollRestoration" in window.history;
-    if (canControlScrollRestoration) {
-      window.history.scrollRestoration = "manual";
-    }
     document.getElementById("root")?.scrollIntoView({ behavior: "smooth" });
   }, [params, pathname]);
 
