cf660d094c533932fda46ba0a07b6690bc181e4b
Mesut Çiftçi <cfcmesut@gmail.com>
2024-01-22 20:28:11 +0300

    test(react-ecom): fix beforeEach function error

--- react-e-commerce-app/src/components/Newsletter/__test__/Newsletter.test.tsx
+++ react-e-commerce-app/src/components/Newsletter/__test__/Newsletter.test.tsx
@@ -1,9 +1,10 @@
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import Newsletter from "..";
-import Navbar from "../../Navbar";
 
 describe("Newsletter", () => {
-  beforeEach(() => render(<Newsletter />));
+  beforeEach(() => {
+    render(<Newsletter />);
+  });
   test("should render the correct text and number of elements", () => {
     const textElement = screen.getByText(
       "Subscribe to our newsletter and receive exclusive offers every week"
