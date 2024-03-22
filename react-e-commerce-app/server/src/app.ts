ae5cb08eaa58a52148e4132dcadb3a75d6065922
Mesut Çiftçi <mesutciftci@Mesut-Ciftci.local>
2024-03-22 23:19:27 +0300

    feat(react-e-commerce/server): created basic product routes

--- react-e-commerce-app/server/src/app.ts
+++ react-e-commerce-app/server/src/app.ts
@@ -5,12 +5,36 @@ const app: Express = express();
 
 const port = 8000;
 
-app.get("/", (req: Request, res: Response) => {
-  res.status(200).json({ message: "Hello from the server" });
+// This enables to use of req.body
+app.use(express.json())
+
+// GET all products
+app.get("/api/v1/products/", (req: Request, res: Response) => {
+  res.status(200).json({ message: "All Products Fetched" });
 });
+
+// GET one product
+app.get("/api/v1/products/:id", (req: Request, res: Response) => {
+  console.log(req.params)
+  res.status(200).json({message: "Product Fetched With Id"})
+})
+
+// Add new product
+app.post("/api/v1/products", (req: Request, res: Response) => {
+  console.log(req.body);
+  res.status(200).json({message: "Product Successfully Created"})
+})
+
+// Update a product
+app.patch("/api/v1/products/:id", (req: Request, res: Response) => {
+  res.status(200).json({message: "Product Successfully Updated"})
+})
+
+// Delete a product
+app.delete("/api/v1/products/:id", (req:Request, res: Response) => {
+  res.status(204).json({message: "Product Successfully Deleted"})
+})
 
 app.listen(port, () => {
   console.log("express init");
 });
-
-console.log("tes221s1t11");
