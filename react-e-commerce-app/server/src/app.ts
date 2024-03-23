e22af9d7cdf8f79f601442c5d031e13a705e5790
Mesut Çiftçi <mesutciftci@Mesut-Ciftci.local>
2024-03-23 12:11:13 +0300

    refactor(react-ecommerce/server): seperate server and express code

--- react-e-commerce-app/server/src/app.ts
+++ react-e-commerce-app/server/src/app.ts
@@ -2,8 +2,6 @@ import express, { Express } from "express";
 import productRouter from "./routes/productRoutes";
 import userRouter from "./routes/userRoutes";
 
-const port = 8000;
-
 // Create express an app instance
 const app: Express = express();
 
@@ -14,7 +12,4 @@ app.use(express.json());
 app.use("/api/v1/products", productRouter);
 app.use("/api/v1/users", userRouter);
 
-// Start And Listen Server
-app.listen(port, () => {
-  console.log("express init");
-});
+export default app;
---
+++ react-e-commerce-app/server/src/server.ts
@@ -0,0 +1,7 @@
+import app from "./app";
+
+// Start And Listen Server
+const port = 8000;
+app.listen(port, () => {
+  console.log("express init");
+});
