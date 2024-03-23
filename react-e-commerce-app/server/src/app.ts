70108671360e09b6ffaca9f6ba1d8cea9aefb3b2
Mesut Çiftçi <mesutciftci@Mesut-Ciftci.local>
2024-03-23 13:45:29 +0300

    style(react-ecommerce/server): implement eslint with prettier

--- react-e-commerce-app/server/src/app.ts
+++ react-e-commerce-app/server/src/app.ts
@@ -1,4 +1,4 @@
-import express, { Express } from "express";
+import express, { type Express } from "express";
 import productRouter from "./routes/productRoutes";
 import userRouter from "./routes/userRoutes";
 
--- react-e-commerce-app/server/src/server.ts
+++ react-e-commerce-app/server/src/server.ts
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 // Start And Listen Server
-const port = process.env.PORT || 8000;
+const port = process.env.PORT != null || 8000;
 app.listen(port, () => {
   console.log("express init", port);
 });
