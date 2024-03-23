1f77a10fa6eaa59b34db8c47097b6094fa2e6bda
Mesut Çiftçi <mesutciftci@Mesut-Ciftci.local>
2024-03-23 10:59:15 +0300

    refactor(react-e-commerce/server): create user routes

--- react-e-commerce-app/server/src/app.ts
+++ react-e-commerce-app/server/src/app.ts
@@ -8,6 +8,7 @@ const port = 8000;
 // This enables to use of req.body
 app.use(express.json())
 
+// Products
 const getAllProducts = (req: Request, res: Response) => {
   res.status(200).json({ message: "All Products Fetched" });
 }
@@ -30,6 +31,31 @@ const deleteProduct = (req:Request, res: Response) => {
   res.status(204).json({message: "Product Successfully Deleted"})
 }
 
+// Users
+const getAllUsers = (req: Request, res: Response) => {
+  res.status(200).json({ message: "All Users Fetched" });
+}
+
+const getUser = (req: Request, res: Response) => {
+  console.log(req.params)
+  res.status(200).json({message: "User Fetched With Id"})
+}
+
+const createUser = (req: Request, res: Response) => {
+  console.log(req.body);
+  res.status(200).json({message: "User Successfully Created"})
+}
+
+const updateUser = (req: Request, res: Response) => {
+  res.status(200).json({message: "User Successfully Updated"})
+}
+
+const deleteUser = (req:Request, res: Response) => {
+  res.status(204).json({message: "User Successfully Deleted"})
+}
+
+
+// Products
 app.route('/api/v1/products')
   .get(getAllProducts)
   .post(createProduct);
@@ -38,6 +64,16 @@ app.route('/api/v1/products')
   .get(getProduct)
   .patch(updateProduct)
   .delete(deleteProduct);
+
+// Users
+app.route('/api/v1/users')
+  .get(getAllUsers)
+  .post(createUser);
+
+  app.route('/api/v1/users/:id')
+  .get(getUser)
+  .patch(updateUser)
+  .delete(deleteUser);
 
 app.listen(port, () => {
   console.log("express init");
