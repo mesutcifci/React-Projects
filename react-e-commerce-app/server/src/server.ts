0ee0368be88548a5bdd7652550fbe420102d9f08
Mesut Çiftçi <mesutciftci@Mesut-Ciftci.local>
2024-03-24 16:23:39 +0300

    feat(react-e-commerce/server): update mongoose to 8.2.3 and configure database connection

--- react-e-commerce-app/server/src/server.ts
+++ react-e-commerce-app/server/src/server.ts
@@ -1,10 +1,25 @@
-import app from "./app";
-import dotenv from "dotenv";
+import mongoose from 'mongoose';
+import app from './app';
+import dotenv from 'dotenv';
 
 dotenv.config();
 
+const DB = process.env.DATABASE?.replace(
+	'<PASSWORD>',
+	process.env.DATABASE_PASSWORD ?? ''
+);
+
+mongoose
+	.connect(DB ?? '')
+	.then(() => {
+		console.log('DB Connected');
+	})
+	.catch((err) => {
+		console.log(err);
+	});
+
 // Start And Listen Server
-const port = process.env.PORT != null || 8000;
+const port = process.env.PORT ?? 8000;
 app.listen(port, () => {
-  console.log("express init", port);
+	console.log('express init', port);
 });
