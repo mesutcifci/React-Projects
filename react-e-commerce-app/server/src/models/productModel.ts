cad4a083944d9bb99635b8f22f2e58e6a781a289
Mesut Çiftçi <mesutciftci@Mesut-Ciftci.local>
2024-03-29 09:07:07 +0300

    refactor(react-e-commerce/server): update product schema and interface

--- react-e-commerce-app/server/src/models/productModel.ts
+++ react-e-commerce-app/server/src/models/productModel.ts
@@ -1,22 +1,72 @@
 import mongoose from 'mongoose';
+import { type IProduct } from '../types/Product';
 
-const productSchema = new mongoose.Schema({
+const productSchema = new mongoose.Schema<IProduct>({
 	name: {
 		type: String,
-		required: [true, 'A Product must have a name'],
+		required: [true, 'A Product Must Have A Name'],
 		unique: true,
+		trim: true,
 	},
 	price: {
 		type: Number,
-		required: [true, 'A Product must have a price'],
+		required: [true, 'A Product Must Have A Price'],
 	},
-	discountPrice: {
-		type: Number,
-		default: 0,
+	discountPrice: Number,
+	currency: {
+		type: String,
+		default: 'USD',
 	},
 	stock: {
 		type: Number,
 		default: 0,
+	},
+	ratingsAverage: {
+		type: Number,
+		default: 5,
+	},
+	ratingsQuantity: {
+		type: Number,
+		default: 0,
+	},
+	description: {
+		type: String,
+		trim: true,
+		required: [true, 'A Product Must Have A Description'],
+	},
+	images: {
+		type: [String],
+		required: [true, 'A Product Must Have An Image'],
+	},
+	badges: {
+		type: [
+			{
+				text: {
+					type: String,
+					required: [true, 'A Badge Must Have An Text Field'],
+				},
+				startDate: {
+					type: Date,
+					required: [true, 'A Badge Must Have A Start Date'],
+				},
+				endDate: {
+					type: Date,
+					required: [true, 'A Badge Must Have A End Date'],
+				},
+			},
+		],
+	},
+	categoryIds: {
+		type: [String],
+		required: [true, 'A Product Must Have A Category ID'],
+	},
+	createdAt: {
+		type: Date,
+		default: Date.now(),
+	},
+	updatedAt: {
+		type: Date,
+		default: Date.now(),
 	},
 });
 
--- react-e-commerce-app/server/src/types/product.ts
+++ react-e-commerce-app/server/src/types/product.ts
@@ -1,6 +1,21 @@
+export interface IBadge {
+	text: string;
+	startDate: Date;
+	endDate: Date;
+}
+
 export interface IProduct {
 	name: string;
 	price: number;
-	discountPrice: number;
+	discountPrice?: number;
+	currency: string;
 	stock: number;
+	ratingsAverage: number;
+	ratingsQuantity: number;
+	description: string;
+	images: [string];
+	badges: [IBadge];
+	categoryIds: [string];
+	createdAt: Date;
+	updatedAt: Date;
 }
