3a483b8bb4fd673798b9d69360f1ac7b8928bd1d
Mesut Çiftçi <mesutciftci@Mesut-Ciftci.local>
2024-03-31 09:46:22 +0300

    feat(react-e-commerce/server): add built in validators, install validator package

--- react-e-commerce-app/server/src/models/productModel.ts
+++ react-e-commerce-app/server/src/models/productModel.ts
@@ -8,12 +8,23 @@ const productSchema = new mongoose.Schema<IProduct>(
 			required: [true, 'A product must have a name'],
 			unique: true,
 			trim: true,
+			maxlength: [150, "A product name can't bigger than 150 character"],
+			minlength: [10, 'A product name must have at least 10 character'],
 		},
 		price: {
 			type: Number,
 			required: [true, 'A product must have a price'],
+			min: [1, 'Price must be bigger than 0'],
 		},
-		discountPrice: Number,
+		discountPrice: {
+			type: Number,
+			validate: {
+				validator: function (value: number) {
+					return value < (this as IProduct).price;
+				},
+				message: "Disocunt price can't bigger than original value",
+			},
+		},
 		currency: {
 			type: String,
 			default: 'USD',
@@ -25,6 +36,8 @@ const productSchema = new mongoose.Schema<IProduct>(
 		ratingsAverage: {
 			type: Number,
 			default: 5,
+			min: [1, 'Rating must be bigger than 0'],
+			max: [5, 'Rating must be less than 5'],
 		},
 		ratingsQuantity: {
 			type: Number,
@@ -34,6 +47,11 @@ const productSchema = new mongoose.Schema<IProduct>(
 			type: String,
 			trim: true,
 			required: [true, 'A product must have a description'],
+			maxlength: [
+				600,
+				'A product description can not bigger than 600 character',
+			],
+			minlength: [10, 'A product description must have at least 10 character'],
 		},
 		images: {
 			type: [String],
@@ -45,6 +63,8 @@ const productSchema = new mongoose.Schema<IProduct>(
 					text: {
 						type: String,
 						required: [true, 'A badge must have an text field'],
+						maxlength: [30, 'A badge text can not bigger than 30 character'],
+						minlength: [10, 'A badge text must have at least 3 character'],
 					},
 					startDate: {
 						type: Date,
