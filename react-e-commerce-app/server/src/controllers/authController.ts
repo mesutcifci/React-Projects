1905b65712cab63b3d2b79cbdb013a4a33753a5c
Mesut Çiftçi <mesutciftci@Mesut-Ciftci.local>
2024-04-18 01:21:53 +0300

    feat(react-e-commerce/server): complete signup logic with jwt

--- react-e-commerce-app/server/src/controllers/authController.ts
+++ react-e-commerce-app/server/src/controllers/authController.ts
@@ -1,16 +1,26 @@
 import catchAsyncErrors from '../helpers/catchAsyncErrors';
 import type { NextFunction, Request, Response } from 'express';
 import userModel from '../models/userModel';
+import jwt from 'jsonwebtoken';
 
 export const signUp = catchAsyncErrors(
 	async (req: Request, res: Response, next: NextFunction): Promise<void> => {
 		const user = await userModel.create(req.body);
 
-		res.status(201).json({
-			status: 'success',
-			data: {
-				user,
-			},
-		});
+		if (process.env.JWT_SECRET) {
+			const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+				expiresIn: process.env.JWT_EXPIRES_IN,
+			});
+
+			res.status(201).json({
+				status: 'success',
+				token,
+				data: {
+					user,
+				},
+			});
+		} else {
+			next('Server Error!');
+		}
 	}
 );
