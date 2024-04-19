6ba6b1a10a95c62c3ae069e7955402537dd26866
Mesut Çiftçi <mesutciftci@Mesut-Ciftci.local>
2024-04-19 21:04:42 +0300

    chore(react-e-commerce): dummy commit

--- react-e-commerce-app/src/firebase.ts
+++ react-e-commerce-app/src/firebase.ts
@@ -15,4 +15,5 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
+
 export const provider = new GoogleAuthProvider();
