ba35414ed0b2c5b2078f01a6412d83fa0a192d23
mesutcifci <cfcmesut@gmail.com>
2024-09-25 21:57:36 +0300

    feat(react-e-commerce/server): handle uncaughtException, unhandledRejection and SIGTERM errors

--- react-e-commerce-app/server/src/server.ts
+++ react-e-commerce-app/server/src/server.ts
@@ -1,6 +1,13 @@
 import mongoose from 'mongoose';
 import app from './app';
 import dotenv from 'dotenv';
+import { type IAppError } from './types/error';
+
+process.on('uncaughtException', (err: IAppError) => {
+	console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
+	console.log(err.name, err.message);
+	process.exit(1);
+});
 
 dotenv.config();
 
@@ -9,17 +16,27 @@ const DB = process.env.DATABASE?.replace(
 	process.env.DATABASE_PASSWORD ?? ''
 );
 
-mongoose
-	.connect(DB ?? '')
-	.then(() => {
-		console.log('DB Connected');
-	})
-	.catch((err) => {
-		console.log(err);
-	});
+void mongoose.connect(DB ?? '', {}).then(() => {
+	console.log('DB Connected');
+});
 
 // Start And Listen Server
 const port = process.env.PORT ?? 8000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
 	console.log('express init', port);
+});
+
+process.on('unhandledRejection', (err: IAppError) => {
+	console.log('UNHANDLED REJECTION! Shutting down...');
+	console.log(err.name, err.message);
+	server.close(() => {
+		process.exit(1);
+	});
+});
+
+process.on('SIGTERM', () => {
+	console.log('👋 SIGTERM RECEIVED. Shutting down gracefully');
+	server.close(() => {
+		console.log('Process terminated!');
+	});
 });
