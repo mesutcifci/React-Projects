4950774fe7c93289af3f06ff5dbcea32838eaed1
mesutcifci <cfcmesut@gmail.com>
2024-11-30 23:05:57 +0300

    build(react-e-commerce/client): set up vitest

--- react-e-commerce-app/client/vite.config.ts
+++ react-e-commerce-app/client/vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/// <reference types="vitest" />
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: ["./vitest-setup.ts"],
+  },
+});
